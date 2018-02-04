2b1a428e5f86e02038463be1a57432bef6fa1694
Nate <nschrein@nmu.edu>
2018-02-04 17:10:12 -0500

    Initial Commit

---
+++ AubreesPizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AubreesPizza
+//
+//  Created by Nate Schreiner on 2/4/18.
+//  Copyright © 2018 Nate Schreiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
